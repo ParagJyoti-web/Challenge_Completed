07884d5dc187e38e705027f950041c0707914174
Parag Jyoti Borah <borahporag810@gmail.com>
2026-09-17 23:10:34 +0530

    Temperature Checker

---
+++ Problem.js
@@ -0,0 +1,18 @@
+function checkTemperature(temperature) { 
+    let message = "";
+  if (temperature < 20) {
+    message = "Cold";
+  } else if (temperature >= 20 && temperature <= 30) {
+    message = "Normal";
+  } else {
+    message = "Hot";
+  }
+return message
+
+}
+
+
+console.log(checkTemperature(-5));
+console.log(checkTemperature(15));
+console.log(checkTemperature(25)); 
+console.log(checkTemperature(35)); 
