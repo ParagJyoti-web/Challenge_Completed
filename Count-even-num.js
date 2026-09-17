c1a42c360c8e6c25b9c2f609e6e5867ec32e6e3d
Parag Jyoti Borah <borahporag810@gmail.com>
2026-09-18 04:28:54 +0530

    Count Even Number

---
+++ Count-even-num.js
@@ -0,0 +1,17 @@
+function countEvenNumbers(numbers) {
+  let count = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    
+    if (numbers[i] % 2!== 0) {
+      continue;
+    }
+    count++;
+  }
+
+  return count;
+}
+
+console.log(countEvenNumbers([3, 8, 11, 14, 20])); 
+console.log(countEvenNumbers([1, 3, 5])); 
+console.log(countEvenNumbers([2, 4, 6])); 
